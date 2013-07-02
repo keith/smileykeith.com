cbde93c59c1d075c5ce35439cd70aa665af8355d
Keith Smiley <keithbsmiley@gmail.com>
2013-07-02 16:09:03 -0400

    Add watch commands

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env rake
 
+# sass --watch _sass:css --style=compressed
+# jekyll server --watch
 task :default => :build
 
 task :build do
   puts %x[sass _sass/style.scss css/style.css --style=compressed]
   puts %x[sass _sass/sdark.scss css/sdark.css --style=compressed]
   puts %x[sass _sass/slight.scss css/slight.css --style=compressed]
+
+  puts %x[jekyll build]
 end
 
