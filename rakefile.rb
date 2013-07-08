4dc0f51711dc81663de67de42fd6e9a04d0970f5
Keith Smiley <keithbsmiley@gmail.com>
2013-07-08 15:33:29 -0400

    Add deploy via rsync rake task

--- rakefile.rb
+++ rakefile.rb
@@ -4,11 +4,34 @@
 # jekyll server --watch
 task :default => :build
 
+desc "Build the site"
 task :build do
+  puts %x[git submodule update --init --recursive]
   puts %x[sass _sass/style.scss css/style.css --style=compressed]
   puts %x[sass _sass/sdark.scss css/sdark.css --style=compressed]
   puts %x[sass _sass/slight.scss css/slight.css --style=compressed]
 
   puts %x[jekyll build]
+end
+
+ssh_user       = "ksmiley@66.175.208.254"
+ssh_port       = "22"
+document_root  = "/srv/www/smileykeith.com/blog"
+
+desc "Deploy website via rsync"
+task :deploy do
+  puts "Deploying via rsync"
+  if !File.exists?('_site')
+    puts "ERROR: You need to run 'rake build' first"
+    exit
+  end
+
+  cmd = "rsync -avze 'ssh -p #{ ssh_port }' --delete #{ public_dir }/ #{ ssh_user }:#{ document_root }"
+
+  if system(cmd)
+    puts "Deployed!"
+  else
+    puts "Failed"
+  end
 end
 
