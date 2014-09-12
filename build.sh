559dc44a1b9726ae79823629661f7ed4ebded08c
Keith Smiley <keithbsmiley@gmail.com>
2014-09-11 22:10:29 -0700

    Build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rake
 
 git clone --branch gh-pages https://github.com/Keithbsmiley/smileykeith.com.git $git_dir
 rm -rf $git_dir/*
-mv _site/* _site/.* $git_dir
+mv _site/* $git_dir
 cd $git_dir; git add --all; git commit -m "`date`"; true
 cd $git_dir; git push
 rm -rf $git_dir
