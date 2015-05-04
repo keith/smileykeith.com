70b7ed5b5ba35565794d26d093b55dd883fe3394
Keith Smiley <keithbsmiley@gmail.com>
2015-05-04 10:53:43 -0700

    Update gems

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ rake
 git clone --branch gh-pages https://github.com/keith/smileykeith.com.git $git_dir
 rm -rf $git_dir/*
 mv _site/* $git_dir
-cd $git_dir; git add --all; git commit -m "`date`"; true
-cd $git_dir; git push
-rm -rf $git_dir
+cd $git_dir; git add --all; git commit -m "$(date)"; true
+# cd $git_dir; git push
+# rm -rf $git_dir
